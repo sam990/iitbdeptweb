abd4112a8ec2ba77bd98123471dd5eaa58e8d38c
nitesh624 <nsinha624@gmail.com>
2019-04-18 09:54:20 +0530

    changes in theme

--- themes/iitb_dj/js/custom.js
+++ themes/iitb_dj/js/custom.js
@@ -48,15 +48,18 @@
   		//there's no secondary nav or secondary nav is below primary nav
   	    if (previousTop - currentTop > scrollDelta) {
   	    	//if scrolling up...
-  	    	mainHeader.removeClass('is-hidden');
-          $(".top-nav").removeClass('top-nav-fixed').addClass('top-nav-hidden');
+          if ( $(window).width() > 767 ){
+  	    	  mainHeader.removeClass('is-hidden');
+            $(".top-nav").removeClass('top-nav-fixed').addClass('top-nav-hidden');
+          }
 
   	    } 
         else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
   	    	//if scrolling down...
-          mainHeader.addClass('is-hidden');
-          $(".top-nav").removeClass('top-nav-hidden').addClass('top-nav-fixed');
-
+          if ( $(window).width() > 767 )
+            mainHeader.addClass('is-hidden');
+            $(".top-nav").removeClass('top-nav-hidden').addClass('top-nav-fixed');
+          }
 		    }
 		  // }
   	}
